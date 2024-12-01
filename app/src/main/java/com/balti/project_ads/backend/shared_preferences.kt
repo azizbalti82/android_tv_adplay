0f3cb7b899663f904fb2f2f1fe61ca7f16c4c162
Aziz Balti <aziz.balti.dev@gmail.com>
2024-12-02 00:14:31 +0100

    added 3 containers in main : 1) home: when the device is connected to server 2) connect: if the device is not connected yet 3) offline: if the device is offline

---
+++ app/src/main/java/com/balti/project_ads/backend/shared_preferences.kt
@@ -0,0 +1,20 @@
+package com.balti.project_ads.backend
+
+import android.content.Context
+import android.content.SharedPreferences
+
+class shared {
+    companion object{
+        fun save_id(context: Context, value: String) {
+            val sharedPreferences: SharedPreferences = context.getSharedPreferences("settings", Context.MODE_PRIVATE)
+            val editor = sharedPreferences.edit()
+            editor.putString("id", value)
+            editor.apply()
+        }
+
+        fun get_id(context: Context): String {
+            val sharedPreferences: SharedPreferences = context.getSharedPreferences("settings", Context.MODE_PRIVATE)
+            return sharedPreferences.getString("id","") ?: ""
+        }
+    }
+}
