75f66eb1fed7bbbb2d5a02695071eb8ee4a31a42
Aziz balti <azizbalti.dev@gmail.com>
2024-12-20 22:20:11 +0100

    adding multiple ads to schedule

--- node.js/src/views/javascript/script.js
+++ node.js/src/views/javascript/script.js
@@ -1096,7 +1096,7 @@ async function addAnotherAd() {
     newSelect.style.marginTop = "5px";
     newSelect.style.marginBottom = "5px";
 
-    await add_ads_in_schedule_droplist('','')
+    await add_ads_in_schedule_droplist('scheduleAdSelect','')
 
     // Insert the new select element before the "Add Another Ad" button
     const addButton = adContainer.querySelector("#the_ad_add_button_to_schedule");
