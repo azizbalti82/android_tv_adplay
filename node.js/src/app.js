6e85243fc5f1f18ff95c261e97ab4d8da2773201
Aziz balti <azizbalti.dev@gmail.com>
2024-12-24 16:14:32 +0100

    bla bla

--- node.js/src/app.js
+++ node.js/src/app.js
@@ -34,7 +34,7 @@ app.use('/images', express.static(path.join(__dirname, 'views', 'images')));
 
 // MongoDB connection
 // "mongodb://mongo:hSjBsOKjnZYHdUUVEXUDUCEMZSCApjDd@junction.proxy.rlwy.net:28709";
-const MONGO_URI ="mongodb://mongo:ODqmUwJPvsQqHOCJFXntVQDAAQfIvgZE@junction.proxy.rlwy.net:21159"
+const MONGO_URI ="mongodb://mongo:YJypphTAdDRXNatoQIAZFfafrtVspcMd@junction.proxy.rlwy.net:29189"
 //const MONGO_URI = "mongodb+srv://azizbalti28:azizbalti123aziz@projectad.vdyro.mongodb.net/?retryWrites=true&w=majority&appName=projectAd";
 
 mongoose.connect(MONGO_URI, {})
