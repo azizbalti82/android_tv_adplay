6e49e428cfe18e8046616237b04231dd10d41348
Aziz balti <azizbalti.dev@gmail.com>
2024-12-25 14:21:19 +0100

    bla bla

--- node.js/src/app.js
+++ node.js/src/app.js
@@ -62,8 +62,20 @@ app.use(routMedia);
 
 //routes ------------------------------------------------------------------------------------------------------------
 app.get('/date', (req, res) => {
-    const currentDateTime = Date.now();
-    res.send(currentDateTime.toString());
+    const now = new Date();
+
+    now.setHours(now.getHours());
+
+    // Get the current date in YYYY-MM-DD format
+    const date = now.toISOString().split('T')[0];
+
+    // Get the current time in HH:mm format
+    const time = now.toISOString().split('T')[1].substring(0, 5);
+
+    // Combine the date and time
+    result =  `${date}T${time}`;
+
+    res.send(result);
 });
 
 
